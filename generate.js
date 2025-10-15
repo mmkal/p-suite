0194b1ff1fbc74d1a0765b6d6dd0c60a28ca6045
Misha Kaletsky <15040698+mmkal@users.noreply.github.com>
2025-10-15 11:17:47 +0100

    try including docs readme

--- generate.js
+++ generate.js
@@ -10,34 +10,113 @@ const generate = async () => {
     const cwd = process.cwd()
     const readme = fs.readFileSync(path.join(cwd, 'readme.md'), 'utf8');
 
-    const packageNames = readme.split('\n')
-        .flatMap(line => {
-            if (!line.startsWith('- **')) return []
-            return [line.split('[')[1].split(']')[0]]
-        })
-    for (const name of packageNames) {
+    const beginGeneratedPackageDocsMarker = '<!-- begin generated package docs -->'
+    const endGeneratedPackageDocsMarker = '<!-- end generated package docs -->'
+    const beginIndex = readme.indexOf(beginGeneratedPackageDocsMarker)
+    const endIndex = readme.indexOf(endGeneratedPackageDocsMarker)
+    if (beginIndex === -1 || endIndex === -1) {
+        throw new Error(`Add ${beginGeneratedPackageDocsMarker} and ${endGeneratedPackageDocsMarker} to the readme somewhere.`)
+    }
+    /** @type {import('./pkgs.json')} */
+    const pkgs = JSON.parse(fs.readFileSync(path.join(cwd, 'pkgs.json'), 'utf8'))
+    
+    const allPkgs = pkgs.sections.flatMap(s => s.packages)
+    for (const pkg of allPkgs) {
+        const name = pkg.name
         try {
             (import.meta.resolve(name))
         } catch {
             await execa('pnpm', ['install', name])
         }
     }
-    const packages = packageNames
-        .map(name => {
+    const packages = allPkgs
+        .map(({name, url}) => {
             const mainModulePath = fileURLToPath(import.meta.resolve(name))
             const mainModuleContent = fs.readFileSync(mainModulePath, 'utf8')
             const pkg = readPackageUpSync({cwd: path.dirname(mainModulePath)})
+            const readmePath = [
+                path.join(path.dirname(pkg.path), 'README.md'),
+                path.join(path.dirname(pkg.path), 'readme.md'),
+            ].find(fs.existsSync)
+            const readmeContent = readmePath ? fs.readFileSync(readmePath, 'utf8') : ''
             return {
+                name,
                 package: name,
+                url,
                 mainModulePath,
                 packageJson: pkg.packageJson,
                 packageJsonPath: pkg.path,
+                readmeContent,
+                readmePath,
                 mainModuleContent,
                 hasDefaultExport: mainModuleContent.includes('export default'),
                 camelCaseName: name.replace(/-(\w)/g, m => m[1].toUpperCase()),
             }
         })
 
+    let newGeneratedReadmeSection = [
+        ...pkgs.sections.map(s => [
+            s.title,
+            s.subtitle ? `\n\n${s.subtitle}\n\n` : '\n\n',
+            s.packages.map(p => {
+                const details = packages.find(d => d.package === p.name)
+                if (!details) {
+                    throw new Error(JSON.stringify({}))
+                }
+                return `- [**${p.name}**](#${p.name}): ${details.packageJson.description}`
+            }).join('\n')
+        ]),
+        '## Docs',
+        ...packages.map(p => {
+            const modifiedContent = p.readmeContent
+                .replaceAll(`# ${p.package}`, ``)
+                .replaceAll('\n#', '\n####')
+                .replaceAll(/#{5,}/g, '#####')
+                .replaceAll(`install ${p.package}`, `install p-suite`)
+                .replaceAll(`from '${p.package}'`, `from 'p-suite/${p.package}'`)
+                .trim()
+            const sections = modifiedContent.split('\n\n')
+            let trimmedContent = ''
+            for (const section of sections) {
+                const newTrimmedContent = [trimmedContent, section].join('\n\n')
+                if (section.startsWith('#') && newTrimmedContent.split('\n').length > 50) {
+                    trimmedContent += `\n\n_see the rest of the docs in the [source package](${p.url})_`
+                    break
+                }
+                trimmedContent = newTrimmedContent
+            }
+            return [
+                `### ${p.package}`,
+                '\n\n',
+                `_Documenation from [source package](${p.url})_`,
+                '\n\n',
+                trimmedContent,
+                '\n\n',
+            ].join('\n')
+        })
+    ].join('\n\n')
+
+    for (const p of packages) {
+        newGeneratedReadmeSection = newGeneratedReadmeSection
+            .replaceAll(`(${p.url})`, `(#${p.name})`)
+    }
+    newGeneratedReadmeSection = newGeneratedReadmeSection
+        .replaceAll(
+            `[More…](https://github.com/sindresorhus/promise-fun)`,
+            `[More…](#packages)`,
+        )
+
+    const updatedReadme = [
+        readme.slice(0, beginIndex),
+        beginGeneratedPackageDocsMarker,
+        '\n',
+        newGeneratedReadmeSection,
+        '\n',
+        readme.slice(endIndex),
+    ]
+
+    fs.writeFileSync(path.join(cwd, 'readme.md'), updatedReadme.join('\n'), 'utf8')
+        
     await execa('pnpm', ['install', ...packages.map(p => `${p.package}@latest`)]);
 
     const {packageJson} = readPackageUpSync({cwd})
